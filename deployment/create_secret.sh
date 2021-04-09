a7f64e63f3883355524fbbb5a2035cc2d0e742ab
gdj0nes <gareth@labelbox.com>
2021-04-09 10:04:56 -0700

    chg: remove k8s templates for now

--- deployment/create_secret.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete secret keys --ignore-not-found
-kubectl create secret generic keys \
-    --from-literal=labelbox_api_key=$LABELBOX_API_KEY \
-    --from-literal=ngrok_key=$NGROK_TOKEN
--- deployment/observe-metrics.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f deployment/observe_manifest.yaml
-kubectl -n observe delete secret credentials --ignore-not-found
-kubectl -n observe create secret generic credentials \
-  --from-literal=customer=$OBSERVE_CUSTOMER_ID \
-  --from-literal=token=$OBSERVE_TOKEN
-
-
